d630fe1028b7c3bf9ed8b34b107ebd7f5e11fc0a
Julien Da Silva <julien@lewagon.org>
2020-12-23 23:56:34 +0100

    ok

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true
 
-  after_create_commit { broadcast_append_to 'posts', partial: 'posts/post_item' }
+  after_create_commit { broadcast_append_to 'posts' }
   after_destroy_commit { broadcast_remove_to 'posts' }
 end
