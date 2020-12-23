98107234769c8a69ecf8ec0d142990cd84ab7447
Julien Da Silva <julien@lewagon.org>
2020-12-23 10:09:57 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_22_202059) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
